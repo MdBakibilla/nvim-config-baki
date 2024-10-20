6f7def3e6d772db7252c7ca94de6aedbcb62fa5b
MdBakibilla <118018012+MdBakibilla@users.noreply.github.com>
2024-10-20 16:37:42 +0600

    mappings.lua

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,24 +8,46 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- Copy: Ctrl+C
-vim.api.nvim_set_keymap("v", "<C-c>", '"+y', { noremap = true, silent = true })
+-- Copy: Ctrl + C
+vim.api.nvim_set_keymap("n", "<C-c>", '"+y', { noremap = true, silent = true }) -- normal mode (yank to clipboard)
+vim.api.nvim_set_keymap("i", "<C-c>", '<Esc>"+yi', { noremap = true, silent = true }) -- insert mode (yank to clipboard)
+vim.api.nvim_set_keymap("v", "<C-c>", '"+y', { noremap = true, silent = true }) -- visual mode (yank to clipboard)
 
--- Paste: Ctrl+V
-vim.api.nvim_set_keymap("n", "<C-v>", '"+p', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "<C-v>", "<C-r>+", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<C-v>", '"+p', { noremap = true, silent = true })
+-- Undo: Ctrl + Z
+vim.api.nvim_set_keymap("n", "<C-z>", "u", { noremap = true, silent = true }) -- normal mode
+vim.api.nvim_set_keymap("i", "<C-z>", "<C-o>u", { noremap = true, silent = true }) -- insert mode
+vim.api.nvim_set_keymap("v", "<C-z>", "u", { noremap = true, silent = true }) -- visual mode
 
--- Undo: Ctrl+Z
-vim.api.nvim_set_keymap("n", "<C-z>", "u", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "<C-z>", "<C-o>u", { noremap = true, silent = true })
+-- Redo: Ctrl + Y (like in Windows)
+vim.api.nvim_set_keymap("n", "<C-y>", "<C-r>", { noremap = true, silent = true }) -- normal mode
+vim.api.nvim_set_keymap("i", "<C-y>", "<C-o><C-r>", { noremap = true, silent = true }) -- insert mode
+vim.api.nvim_set_keymap("v", "<C-y>", "<C-r>", { noremap = true, silent = true }) -- visual mode
 
--- Redo: Ctrl+Y (like in Windows)
-vim.api.nvim_set_keymap("n", "<C-y>", "<C-r>", { noremap = true, silent = true })
+-- Select all: Ctrl + A
+vim.api.nvim_set_keymap("n", "<C-a>", "ggVG", { noremap = true, silent = true }) -- normal mode
+vim.api.nvim_set_keymap("v", "<C-a>", "<Esc>ggVG", { noremap = true, silent = true }) -- visual mode
+vim.api.nvim_set_keymap("i", "<C-a>", "<C-o>ggVG", { noremap = true, silent = true }) -- insert mode
 
--- Select all: Ctrl+A
-vim.api.nvim_set_keymap("n", "<C-a>", "ggVG", { noremap = true, silent = true })
-
+-- Autocommands for Python
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  callback = function()
+    vim.api.nvim_buf_set_keymap(
+      0,
+      "n",
+      "<F5>",
+      ":w<CR>:exec '!python3' shellescape(@%, 1)<CR>",
+      { noremap = true, silent = true }
+    )
+    vim.api.nvim_buf_set_keymap(
+      0,
+      "i",
+      "<F5>",
+      "<Esc>:w<CR>:exec '!python3' shellescape(@%, 1)<CR>",
+      { noremap = true, silent = true }
+    )
+  end,
+})
 -- Autocommands for Python
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "python",
